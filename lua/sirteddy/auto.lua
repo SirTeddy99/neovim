488c5b305008c11ec6a77f811a4e8896de17409d
Røysland, Tor Edvard <TorEdvard.Roysland@skatteetaten.no>
2024-08-01 10:49:13 +0200

    1

---
+++ lua/sirteddy/auto.lua
@@ -0,0 +1,10 @@
+-- ~/.config/nvim/lua/sirteddy/tflint.lua
+
+-- Run tflint on save
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--  pattern = "*.tf",
+--  callback = function()
+--    vim.cmd("TFLint")
+-- end,
+--})
+
