4350bcaeeff6a0056e5cf9c749e4e275b94ea976
Røysland, Tor Edvard <TorEdvard.Roysland@skatteetaten.no>
2024-10-07 09:50:32 +0200

    comment on toggleterm config

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,6 +1,6 @@
 require("toggleterm").setup{
     size = 20,
-    open_mapping = [[<C-t>]],
+    open_mapping = [[<C-t>]], -- toggle map
     shading_factor = 2,
     direction = 'float', -- Bytte denne til 'vertical' | 'horizontal' | 'tab' | 'float'
     float_opts = {
