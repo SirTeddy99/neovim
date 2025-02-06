48de7cbc5fd3dad0aeabc98ae3e89a33635f9682
Røysland, Tor Edvard <toredvard.roysland@skatteetaten.no>
2025-02-06 10:40:10 +0100

    updated fugitive

--- lua/sirteddy/lazy/fugitive.lua
+++ lua/sirteddy/lazy/fugitive.lua
@@ -1,10 +1,13 @@
 return {
     "tpope/vim-fugitive",
     config = function()
+        -- Default Git command mapping
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
+        -- Create an autogroup for Fugitive
         local SirTeddy_Fugitive = vim.api.nvim_create_augroup("SirTeddy_Fugitive", {})
 
+        -- Create an autocommand for Fugitive buffers
         local autocmd = vim.api.nvim_create_autocmd
         autocmd("BufWinEnter", {
             group = SirTeddy_Fugitive,
@@ -16,23 +19,28 @@ return {
 
                 local bufnr = vim.api.nvim_get_current_buf()
                 local opts = {buffer = bufnr, remap = false}
+
+                -- Git push command
                 vim.keymap.set("n", "<leader>p", function()
                     vim.cmd.Git('push')
                 end, opts)
 
-                -- rebase always
+                -- Git pull with rebase
                 vim.keymap.set("n", "<leader>P", function()
                     vim.cmd.Git({'pull',  '--rebase'})
                 end, opts)
 
-                -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-                -- needed if i did not set the branch up correctly
+                -- Git push with tracking branch
                 vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
             end,
         })
 
-
+        -- Diffget mappings
         vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
         vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+
+        -- Keybinding for switching to 'main' and pulling (Ctrl+M)
+        vim.keymap.set("n", "<C-M>", ':Git checkout main | Git pull --ff-only<CR>', { noremap = true, silent = true })
     end
 }
+
