7972b733bb6598dc58eb1b6b461a0f7f6ad4155d
Tor Edvard <tor.edvard.roysland@netpower.no>
2025-10-31 14:37:58 +0100

    remove groovy from dependend

--- lua/sirteddy/lazy/lsp.lua
+++ lua/sirteddy/lazy/lsp.lua
@@ -33,7 +33,7 @@ return {
 		require("mason-lspconfig").setup({
 			ensure_installed = {
 				"lua_ls", -- Lua Language Server
-				"groovyls",
+				-- "groovyls",
 				"powershell_es",
 			},
 			handlers = {
